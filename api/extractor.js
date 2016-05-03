e7ce084f36dd5fdc322b6ea651107bd6e610ca5b
zur Tene <zurtene@gmail.com>
2016-05-03 15:04:02 +0300

    mc

--- api/extractor.js
+++ api/extractor.js
@@ -1,16 +1,17 @@
 'use strict';
-let http = require('http');
-let data = "";
-http.get({
-    hostname: 'www.next.co.uk',
-    path: '/clearance/?department=womenswear',
-    port: 80
-}, (res)=>{
-    res.on('data', (chunk)=>{
-        console.log(chunk);
-        data += chunk;
-    });
-    res.on('end', ()=>{
-        console.log(data.toString())
-    });
-});
+//let http = require('http');
+//let data = "";
+//http.get({
+//    hostname: 'www.next.co.uk',
+//    path: '/clearance/?department=womenswear',
+//    port: 80
+//}, (res)=>{
+//    res.on('data', (chunk)=>{
+//        console.log(chunk);
+//        data += chunk;
+//    });
+//    res.on('end', ()=>{
+//        console.log(data.toString())
+//    });
+//});
+
